6ebefde394ca63a74efae6e9b29c76ad16cf8566
Hema Srikar <hemasrikar.ankem@gmail.com>
2026-07-26 03:26:24 +0100

    Added: Orthongonal test script slurm

---
+++ slurm/transformer_analysis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --partition=gpu
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem-per-cpu=5960
+#SBATCH --gres=gpu:lovelace_l40:1
+#SBATCH --time=48:00:00
+#SBATCH --output=logs-transformer-analysis/orthogonal_%j.out
+#SBATCH --error=logs-transformer-analysis/orthogonal_%j.err
+
+module --force purge
+
+cd /springbrook/share/wbs/bstvvz
+
+# activate venv
+source .venv/bin/activate
+
+mkdir -p logs-transformer-analysis
+python src/score_orthogonality_test.py
